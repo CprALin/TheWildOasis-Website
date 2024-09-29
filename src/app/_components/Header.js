db5dd3e2723dc6b95f9caf39b573c2d08842fc19
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-09-29 21:04:11 +0300

    #

--- src/app/_components/Header.js
+++ src/app/_components/Header.js
@@ -1,5 +1,5 @@
-import Navigation from '@/app/_components/Navigation';
-import Logo from '@/app/_components/Logo';
+import Navigation from '@/src/app/_components/Navigation';
+import Logo from '@/src/app/_components/Logo';
 
 function Header() {
   return (
