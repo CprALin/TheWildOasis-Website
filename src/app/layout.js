869e6a4777e75f1d8282643cf795de06ea4db279
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-09-29 21:38:03 +0300

    #

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,3 @@
-import Logo from "@/src/app/_components/Logo";
-import Navigation from "@/src/app/_components/Navigation";
 import "@/src/app/_styles/globals.css";
 
 import {Josefin_Sans} from "next/font/google";
@@ -24,7 +22,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}>
+      <body className={`${josefin.className} antialiased bg-primary-950 
+        text-primary-100 min-h-screen flex flex-col`}>
         
         <Header />
 
