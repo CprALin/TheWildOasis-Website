412d2a7815457079e68991b6da15af342b8870e3
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-11-14 21:00:50 +0200

    #

--- src/app/_components/DateSelector.js
+++ src/app/_components/DateSelector.js
@@ -3,6 +3,7 @@
 import { isWithinInterval } from "date-fns";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
+import { useReservation } from "./ReservationContext";
 
 function isAlreadyBooked(range, datesArr) {
   return (
@@ -14,25 +15,28 @@ function isAlreadyBooked(range, datesArr) {
   );
 }
 
-function DateSelector() {
+function DateSelector({settings , cabin , bookedDates}) {
+
+  const { range , setRange } = useReservation(); 
+
   // CHANGE
   const regularPrice = 23;
   const discount = 23;
   const numNights = 23;
   const cabinPrice = 23;
-  const range = { from: null, to: null };
 
   // SETTINGS
-  const minBookingLength = 1;
-  const maxBookingLength = 23;
+  const { min_booking_length , max_booking_length } = settings;
 
   return (
     <div className="flex flex-col justify-between">
       <DayPicker
         className="pt-12 place-self-center"
         mode="range"
-        min={minBookingLength + 1}
-        max={maxBookingLength}
+        onSelect={setRange(range)}
+        selected={range}
+        min={min_booking_length + 1}
+        max={max_booking_length}
         fromMonth={new Date()}
         fromDate={new Date()}
         toYear={new Date().getFullYear() + 5}
