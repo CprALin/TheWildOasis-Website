079eed9890a8ea6f56417fa9ed2c02fa8b1eb5f8
Dragomir Alin - Ciprian <96480829+CprALin@users.noreply.github.com>
2024-11-21 22:12:59 +0200

    #

--- src/app/account/reservations/edit/[bookingId]/page.js
+++ src/app/account/reservations/edit/[bookingId]/page.js
@@ -1,27 +1,36 @@
-export default function Page() {
-  // CHANGE
-  const reservationId = 23;
-  const maxCapacity = 23;
+
+import SubmitButton from "@/src/app/_components/SubmitButton";
+import { updateBooking } from "@/src/app/_lib/actions";
+import { getBooking, getCabin } from "@/src/app/_lib/data-service";
+
+export default async function Page({params}) {
+  
+  const {bookingId} = await params;
+  const {num_guests , observations, cabin_id} = await getBooking(bookingId);
+  const {max_capacity} = await getCabin(cabin_id);
 
   return (
     <div>
       <h2 className="font-semibold text-2xl text-accent-400 mb-7">
-        Edit Reservation #{reservationId}
+        Edit Reservation #{bookingId}
       </h2>
 
-      <form className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
+      <form action={updateBooking} className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
+        <input type="hidden" value={bookingId} name="bookingId" /> 
+
         <div className="space-y-2">
           <label htmlFor="numGuests">How many guests?</label>
           <select
-            name="numGuests"
-            id="numGuests"
+            name="num_guests"
+            id="num_guests"
+            defaultValue={num_guests}
             className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
             required
           >
             <option value="" key="">
               Select number of guests...
             </option>
-            {Array.from({ length: maxCapacity }, (_, i) => i + 1).map((x) => (
+            {Array.from({ length: max_capacity }, (_, i) => i + 1).map((x) => (
               <option value={x} key={x}>
                 {x} {x === 1 ? "guest" : "guests"}
               </option>
@@ -35,14 +44,13 @@ export default function Page() {
           </label>
           <textarea
             name="observations"
+            defaultValue={observations}
             className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
           />
         </div>
 
         <div className="flex justify-end items-center gap-6">
-          <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-            Update reservation
-          </button>
+            <SubmitButton pendingLabel="Updating...">Update profile</SubmitButton>
         </div>
       </form>
     </div>
